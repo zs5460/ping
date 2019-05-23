58e1d6ab9c9b449f58957177dddd3ad0d7e8f7bb
zs5460 <zs5460@gmail.com>
2019-05-23 21:54:48 +0800

    add SendMail

--- main.go
+++ main.go
@@ -5,17 +5,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/smtp"
+	"strings"
 	"sync"
 
 	"github.com/paulstuart/ping"
 )
 
+var cfg *Config
+
 // Config ...
 type Config struct {
+	MailSubject   string
 	MailServer    string
 	MailSender    string
 	MailSenderPwd string
-	MailReciver   []string
+	MailReciver   string
 	IPList        []string
 }
 
@@ -33,21 +38,47 @@ func ReadConfig() *Config {
 	return &config
 }
 
+// SendMail ...
+func SendMail(user, password, host, to, subject, body string) error {
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", user, password, hp[0])
+	contentType := "Content-Type: text/html; charset=UTF-8"
+	msg := []byte("To: " + to + "\r\nFrom: " + user +
+		"\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + body)
+	sendTo := strings.Split(to, ";")
+	err := smtp.SendMail(host, auth, user, sendTo, msg)
+	return err
+}
+
+// SendReport ...
+func SendReport(msg string) error {
+	return SendMail(
+		cfg.MailSender,
+		cfg.MailSenderPwd,
+		cfg.MailServer,
+		cfg.MailReciver,
+		cfg.MailSubject, msg)
+}
+
 // PingHost ...
 func PingHost(ip string) {
 	if ping.Ping(ip, 2) {
 		fmt.Printf("ping %s success!\n", ip)
 	} else {
-		fmt.Printf("ping %s failed!\n", ip)
+		fmt.Printf("ping %s timeout!\n", ip)
 		//log to file
 
 		// send mail
+		err := SendReport(ip)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
 func main() {
 
-	cfg := ReadConfig()
+	cfg = ReadConfig()
 	fmt.Printf("%#v\n", cfg)
 
 	iplist := cfg.IPList
